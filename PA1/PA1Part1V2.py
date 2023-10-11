d3a0db0824ad9544250c418ce89817a2219b24ee
Brandon Cartwright <bcartwright16@gmail.com>
2023-10-10 18:48:19 -0600

    Cleaned up code

--- PA1/PA1Part1V2.py
+++ PA1/PA1Part1V2.py
@@ -10,6 +10,32 @@ class Node:
         self.cost = cost
         self.depth = depth
 
+def results(end_node, nodes_expanded, max_nodes, elapsed_time):
+    if end_node:
+        total_cost = 0
+        path = []
+        while end_node:
+            path.append(end_node.coord)
+            total_cost += end_node.cost
+            end_node = end_node.parent
+        path = path[::-1]
+        print("Path Found!")
+        print("Total Cost:", total_cost)
+        print("Number of nodes expanded:", nodes_expanded)
+        print("Max nodes stored in memory:", max_nodes)
+        print("Total time: {:.9f} seconds".format(elapsed_time))
+        print("Path sequence:", path)
+    else:
+    
+        print("No path found.")
+        print("Number of nodes expanded:", nodes_expanded)
+        print("Max nodes stored in memory:", max_nodes)
+        print("Total time: {:.9f} seconds".format(elapsed_time))
+
+
+def aStar():
+    return
+
 def bfs(dimensions, start, goal, map_grid):
     startTime = time.time()
     rows, cols = dimensions
@@ -23,7 +49,7 @@ def bfs(dimensions, start, goal, map_grid):
 
     while queue:
         elapsedTime = time.time() - startTime
-        if elapsedTime > 180:  # 180 seconds = 3 minutes
+        if elapsedTime > timer:  # 180 seconds = 3 minutes
             print("3-minute time cutoff reached!")
             return None, nodes_expanded, max_nodes
         max_nodes = max(max_nodes, len(queue))
@@ -56,7 +82,7 @@ def dfs(dimensions, start, goal, map_grid,depth):
 
     while stack:
         elapsedTime = time.time() - startTime
-        if elapsedTime > 180:  # 180 seconds = 3 minutes
+        if elapsedTime > timer:  # 180 seconds = 3 minutes
             print("3-minute time cutoff reached!")
             return None, nodes_expanded, max_nodes
         
@@ -99,39 +125,12 @@ def file_reader():
         return None, None, None, None
 
 
-
-
-def main():
-    dimensions, start, goal, map_grid = file_reader()
-    if not dimensions or not start or not goal or not map_grid:
-        return
-    searchChoice = input("Enter search algorithm (bfs, ids, a*):")
+def searchDecision(searchChoice, dimensions, start, goal, map_grid):
     if searchChoice == "bfs":
         start_time = time.time()
         end_node, nodes_expanded, max_nodes = bfs(dimensions, start, goal, map_grid)
         end_time = time.time()
-
-        if end_node:
-            total_cost = 0
-            path = []
-            while end_node:
-                path.append(end_node.coord)
-                total_cost += end_node.cost
-                end_node = end_node.parent
-            path = path[::-1]
-            print("Path Found!")
-            print("Total Cost:", total_cost)
-            print("Number of nodes expanded:", nodes_expanded)
-            print("Max nodes stored in memory:", max_nodes)
-            print("Total time: {:.9f} seconds".format(end_time - start_time))
-            print("Path sequence:", path)
-        else:
-        
-            print("No path found.")
-            print("Number of nodes expanded:", nodes_expanded)
-            print("Max nodes stored in memory:", max_nodes)
-            print("Total time: {:.9f} seconds".format(end_time - start_time))
-    if searchChoice == "ids":
+    elif searchChoice == "ids":
         depth = 0
         start_time = time.time()
         while True:
@@ -149,31 +148,26 @@ def main():
                 break
 
         end_time = time.time()
-
-        if end_node:
-            total_cost = 0
-            path =[]
-            while end_node:
-                path.append(end_node.coord)
-                total_cost += end_node.cost
-                end_node = end_node.parent
-            path = path[::-1]
-            print("Path Found!")
-            print("Total Cost:", total_cost)
-            print("Number of nodes expanded:", nodes_expanded)
-            print("Max nodes stored in memory:", max_nodes)
-            print("Total time: {:.9f} seconds".format(end_time - start_time))
-            print("Path sequence:", path)
-        else:
-            print("No path found.")
-            print("Number of nodes expanded:", nodes_expanded)
-            print("Max nodes stored in memory:", max_nodes)
-            print("Total time: {:.9f} seconds".format(end_time - start_time))
+    elif searchChoice == "a*":
+        return
     else:
         print("Search has not been implemented")
+        return
     
+    elapsed_time = time.time() - start_time
+    results(end_node, nodes_expanded, max_nodes, elapsed_time)
 
 
+
+def main():
+    global timer
+    timer = 180
+    dimensions, start, goal, map_grid = file_reader()
+    if not dimensions or not start or not goal or not map_grid:
+        print("File not in correct format")
+        return
+    searchChoice = input("Enter search algorithm (bfs, ids, a*):")
+    searchDecision(searchChoice, dimensions, start, goal, map_grid)
     
 
 if __name__ == "__main__":
--- README.md
+++
@@ -1,5 +0,0 @@
-# PA1 Part 1
-
-For part 1 of this assignment, we have only implemented BFS by using a node class and some helper methods to aid with reading the test files and printing out relevant information. This code is written in Python.
-
-To run this, you can use a standard "python3 PA1Part1V2.py" command. Once running, the program will ask which test file you would like to run i.e. "Test1.txt".
